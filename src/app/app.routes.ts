778884fa2eac7def4b60b245d53b4240d9839da4
snehakethineni <skethineni095@gmail.com>
2025-01-17 10:23:39 -0500

    added dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginPanelComponent } from './login-panel/login-panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'login',
     component: LoginPanelComponent,
   },
 ];
